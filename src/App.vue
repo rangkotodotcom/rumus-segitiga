01e56492e8f3980e42e6612382aaadcee227b443
rangkotodotcom <softj4m98@gmail.com>
2023-05-08 21:40:44 +0700

    intial commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import RumusSegitiga from './components/RumusSegitiga.vue'
+</script>
+
+<template>
+  <main>
+    <RumusSegitiga />
+  </main>
+</template>
+
---
+++ src/components/RumusSegitiga.vue
@@ -0,0 +1,35 @@
+<template>
+    Alas : <input type="text" v-model="alas" v-on:keyup="hitung" placeholder="Alas">
+    <br>
+    Tinggi : <input type="text" v-model="tinggi" v-on:keyup="hitung" placeholder="Tinggi">
+    <br>
+    <br>
+    Menghitung Luas Segitiga:
+    <br>
+
+    Alas : {{ alas }}
+    <br>
+    Tinggi: {{ tinggi }}
+    <br>
+    Luas : {{ luas }}
+</template>
+
+<script>
+export default {
+    data() {
+        //Isiniasi variabel
+        return {
+            alas: null,
+            tinggi: null,
+            luas: null
+        };
+    },
+    methods: {
+        // function menghitung luas
+        hitung() {
+            this.luas = (this.alas * this.tinggi) / 2
+        }
+    }
+
+}
+</script>
