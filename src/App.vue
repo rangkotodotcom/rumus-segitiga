a505a0901c35bdef2d23fa4d0cb91f5b28c438fe
rangkotodotcom <softj4m98@gmail.com>
2023-05-09 20:19:03 +0700

    add v directive

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <script setup>
 import RumusSegitiga from './components/RumusSegitiga.vue'
+import VDirective from './components/VDirective.vue'
 </script>
 
 <template>
   <main>
     <RumusSegitiga />
+    <br>
+    <br>
+    <br>
+    <VDirective />
   </main>
 </template>
 
---
+++ src/components/VDirective.vue
@@ -0,0 +1,23 @@
+<template>
+    Email : <input type="text" v-bind:value="email" placeholder="Email">
+    <br>
+    Nama : <input type="text" v-model="nama" placeholder="Nama">
+    <br>
+    <br>
+
+    Email : {{ email }}
+    <br>
+    Nama: {{ nama }}
+</template>
+
+<script>
+export default {
+    data() {
+        //Isiniasi variabel
+        return {
+            email: "email@domain.com",
+            nama: null
+        };
+    },
+}
+</script>
